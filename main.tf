87bfa41529121bc50f59ff52dd5517bc9217f12f
milh0use <steven.hirchorn@gmail.com>
2020-12-03 13:58:41 +0000

    Move iterating functionality to parent module

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "aws_iam_role_policy" "role_policy" {
-  name = var.role_and_policy.role_policy_name
+  name = var.role_policy_name
   role = aws_iam_role.role.id
-  policy = var.role_and_policy.role_policy
+  policy = var.role_policy
 }
 
 resource "aws_iam_role" "role" {
-  name = var.role_and_policy.role_name
-  assume_role_policy = var.role_and_policy.assume_role_policy
+  name = var.role_name
+  assume_role_policy = var.assume_role_policy
 }
--- vars.tf
+++ vars.tf
@@ -3,4 +3,7 @@ variable "environment" {}
 variable "owner" {}
 variable "created_by" {}
 
-variable "role_and_policy" {}
+variable "role_name" {}
+variable "assume_role_policy" {}
+variable "role_policy" {}
+variable "role_policy_name" {}
