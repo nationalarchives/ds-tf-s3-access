8536204b1b50e25a732c35217cc3398e667ba2d2
milh0use <steven.hirchorn@gmail.com>
2020-12-03 16:37:21 +0000

    Add tags to IAM Role

--- main.tf
+++ main.tf
@@ -7,4 +7,11 @@ resource "aws_iam_role_policy" "role_policy" {
 resource "aws_iam_role" "role" {
   name = var.role_and_policy.role_name
   assume_role_policy = var.role_and_policy.assume_role_policy
+  tags = {
+    Name = var.role_and_policy.role_name
+    Environment = var.environment
+    Account = var.account
+    Owner = var.owner
+    CreatedBy = var.created_by
+  }
 }
