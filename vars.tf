eb22310a12ed130b8cec25217712c3c6cce5c44e
milh0use <steven.hirchorn@gmail.com>
2020-12-03 15:36:21 +0000

    Try using map input variable

--- vars.tf
+++ vars.tf
@@ -3,7 +3,9 @@ variable "environment" {}
 variable "owner" {}
 variable "created_by" {}
 
-variable "role_name" {}
-variable "assume_role_policy" {}
-variable "role_policy" {}
-variable "role_policy_name" {}
+//variable "role_name" {}
+//variable "assume_role_policy" {}
+//variable "role_policy" {}
+//variable "role_policy_name" {}
+
+variable "role_and_policy" {}
